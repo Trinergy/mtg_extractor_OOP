090726024a9aaf31e21befc732baddefbe70dbfe
Kenny Wu <wu.kenny1221@gmail.com>
2016-06-20 22:01:32 -0700

    add converter

--- lib/app.rb
+++ lib/app.rb
@@ -4,6 +4,7 @@ require "docsplit" #convert file to text with or w/o OCR
 #http://documentcloud.github.io/docsplit/#usage
 require "mimemagic" #determine mime type by file content
 #https://github.com/minad/mimemagic
+require "pry"
 require_relative "converter"
 
 p Converter.call(Dir["./docs/files/*"])
